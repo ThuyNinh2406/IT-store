9d6d5e6abf829aba37800a2eac015a8cfd420bf5
521951060942NguyenHaiQuan <nguyenhaiquan25052001@gmail.com>
2022-06-02 15:53:03 +0700

    first project

--- app/src/main/java/com/example/it_store/Home.java
+++ app/src/main/java/com/example/it_store/Home.java
@@ -1,14 +1,106 @@
 package com.example.it_store;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.it_store.adapter.LoaispAdapter;
+import com.example.it_store.model.Loaisp;
+import com.example.it_store.ultil.CheckConnection;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewmanhinhchinh;
+    NavigationView navigationView;
+    ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
+    ArrayList<Loaisp> mangloaisp;
+    LoaispAdapter loaispAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Anhxa();
+        if(CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionBar();
+            ActionViewFlipper();
+        }
+        else{
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Bạn hãy kiểm tra lại kết nối");
+        }
     }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/5/17/637883852147874927_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/5/30/637895318671566801_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/5/26/637891757782724394_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/12/17/637753295458679666_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/4/25/637864735735607617_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/5/26/637891757782724394_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2022/5/30/637895318671566801_F-C1_1200x300.png");
+        mangquangcao.add("https://images.fpt.shop/unsafe/fit-in/1200x300/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2021/12/14/637750929820398764_F-C1_1200x300.png");
+
+
+        for(int i=0;i<mangquangcao.size();i++){
+            ImageView imageView= new ImageView(getApplicationContext());
+            Picasso.get().load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation animation_slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(animation_slide_in);
+        viewFlipper.setOutAnimation(animation_slide_out);
+    }
+
+    private void ActionBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+    private void Anhxa() {
+        toolbar=(Toolbar) findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper= (ViewFlipper) findViewById(R.id.viewlipper);
+        recyclerViewmanhinhchinh=(RecyclerView) findViewById(R.id.recyclerview);
+        navigationView=(NavigationView) findViewById(R.id.navigationview);
+        listViewmanhinhchinh= findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawerlayout);
+        mangloaisp = new ArrayList<>();
+        loaispAdapter = new LoaispAdapter(mangloaisp,getApplicationContext());
+        listViewmanhinhchinh.setAdapter(loaispAdapter);
+    }
+
 }
